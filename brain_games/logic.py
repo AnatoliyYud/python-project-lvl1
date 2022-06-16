0650ce07c9ab7dda2524c1832e40fe9789300ed9
Anatoliy Yudaev <trouble93@yandex.ru>
2022-06-16 14:15:59 +0300

    end of 5 lesson. part2

---
+++ brain_games/logic.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env puthon3
+def game_logic():
+    import prompt
+    name = prompt.string('May I have your name? ')
+    print('Hello,', name + '!')
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    i = 1
+    while i <= 3:
+        import random
+        digit = random.randint(1, 1000)
+        print('Question:', digit)
+        answer = prompt.string('Your answer: ')
+        if answer == 'yes' and digit % 2 == 0:
+            print('Correct!')
+            if i == 3:
+                print('Congratulations,', name + '!')
+                break
+            else:
+                i += 1
+        elif answer == 'no' and digit % 2 != 0:
+            print('Correct!')
+            if i == 3:
+                print('Congratulations,', name + '!')
+                break
+            else:
+                i += 1
+        elif answer == 'yes' and digit % 2 != 0:
+            print("'yes' is wrong answer ;(. Correct answer was 'no'.")
+            print("Let's try again,", name + '!')
+            break
+        elif answer == 'no' and digit % 2 == 0:
+            print("'yes' is wrong answer ;(. Correct answer was 'no'.")
+            print("Let's try again,", name + '!')
+            break
+        elif answer != 'yes' or answer != 'no':
+            print("Please answer 'yes' or 'no'!")
+            print("Let's try again,", name + '!')
+            break
