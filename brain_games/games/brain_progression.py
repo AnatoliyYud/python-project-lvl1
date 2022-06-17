9883b7ee0cf264784d3669cca4dbef907c472d98
Anatoliy Yudaev <trouble93@yandex.ru>
2022-06-17 15:20:54 +0300

    new def in logic.py and new script brain_progression

---
+++ brain_games/games/brain_progression.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from brain_games.logic import game_progression
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    game_progression()
+
+
+if __name__ == '__main__':
+    main()
--- brain_games/logic.py
+++ brain_games/logic.py
@@ -113,3 +113,36 @@ def game_gcd():
             print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
             print(f"Let's try again, {name}!")
             break
+
+def game_progression():
+    import random, prompt, math
+    name = prompt.string('May I have your name? ')
+    print(f"Hello, {name}!")
+    print('What number is missing in the progression?')
+    i = 1
+    while i <= 3:
+        first_digit = random.randint(0, 50)
+        step = random.randint(1, 50)
+        result = [first_digit,]
+        digit = first_digit
+        n = 1
+        while n < 10:
+            digit += step
+            result.append(digit)
+            n += 1
+        correct_answer = random.choice(result)
+        position = result.index(correct_answer)
+        result[position] = '..'
+        print('Question:', *result)
+        answer = prompt.integer('Your answer: ',)
+        if answer == correct_answer and i != 3:
+            print('Correct!')
+            i += 1
+        elif answer == correct_answer and i == 3:
+            print('Correct!')
+            print(f"Congratulations, {name}!")
+            break
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {name}!")
+            break
