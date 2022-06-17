d59bdb040b88ecf0b3eb7bdc6b006987bdb63bec
Anatoliy Yudaev <trouble93@yandex.ru>
2022-06-17 11:21:20 +0300

    modify brain_games.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from brain_games.logic import game_logic
 
+
 def main():
     print('Welcome to the Brain Games!')
     game_logic()
@@ -8,4 +9,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
