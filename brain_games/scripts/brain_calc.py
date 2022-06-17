437bd87ec05c659cb4edab1a68ed110383d1d9ab
Anatoliy Yudaev <trouble93@yandex.ru>
2022-06-17 11:44:39 +0300

    add new script brain_games/scripts/brain_calc.py

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from brain_games.logic import game_calc
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    game_calc()
+
+
+if __name__ == '__main__':
+    main()
