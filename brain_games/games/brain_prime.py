6a03b43ae4374a5d831036469b2577a0c1a263ea
Anatoliy Yudaev <trouble93@yandex.ru>
2022-06-17 18:54:58 +0300

    add logic in logic file and add new brain_prime.py

---
+++ brain_games/games/brain_prime.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from brain_games.logic import game_prime
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    game_prime()
+
+
+if __name__ == '__main__':
+    main()
--- brain_games/logic.py
+++ brain_games/logic.py
@@ -146,3 +146,39 @@ def game_progression():
             print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
             print(f"Let's try again, {name}!")
             break
+
+def game_prime():
+    import random, prompt
+    name = prompt.string('May I have your name? ')
+    print(f"Hello, {name}!")
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
+    i = 1
+    while i <= 3:
+        digit = random.randint(1, 50)
+        print('Question:', digit)
+        count = 0
+        for n in range(1, digit // 2 + 1):
+            if digit % n == 0:
+                count += 1
+        if count == 1:
+            correct_answer = 'yes'
+        if count != 1:
+            correct_answer = 'no'
+        answer = prompt.string('Your answer: ')
+        if answer != 'yes' and answer != 'no':
+            print("Please answer 'yes' or 'no'!")
+            print("Let's try again,", name + '!')
+            break
+        else:
+            if answer == correct_answer and i != 3:
+                print('Correct!')
+                i += 1
+                continue
+            elif answer == correct_answer and i == 3:
+                print('Correct!')
+                print(f"Congratulations, {name}!")
+                break
+            elif answer != correct_answer:
+                print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+                print("Let's try again,", name + '!')
+                break
