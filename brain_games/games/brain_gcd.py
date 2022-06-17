aead5b8426a97839a1ac8bdc5f873ac4423f9c4b
Anatoliy Yudaev <trouble93@yandex.ru>
2022-06-17 12:57:25 +0300

    new add in brain_games/logic.py and new script brain_games/games/brain_calc.py

---
+++ brain_games/games/brain_gcd.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from brain_games.logic import game_gcd
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    game_gcd()
+
+
+if __name__ == '__main__':
+    main()
--- brain_games/logic.py
+++ brain_games/logic.py
@@ -89,3 +89,27 @@ def game_calc():
                 print(f"'{str(answer)}' is wrong answer ;(. Correct answer was '{str(correct_answer)}'.")
                 print(f"Let's try again, {name}!")
                 break
+
+def game_gcd():
+    import random, prompt, math
+    name = prompt.string('May I have your name? ')
+    print(f"Hello, {name}!")
+    print('Find the greatest common divisor of given numbers.')
+    i = 1
+    while i <= 3:
+        first_digit = random.randint(1, 100)
+        second_digit = random.randint(1, 100)
+        correct_answer = math.gcd(first_digit, second_digit)
+        print(f"Question: {first_digit} {second_digit}")
+        answer = prompt.integer('Your answer: ',)
+        if answer == correct_answer and i != 3:
+            print('Correct!')
+            i += 1
+        elif answer == correct_answer and i == 3:
+            print('Correct!')
+            print(f"Congratulations, {name}!")
+            break
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {name}!")
+            break
